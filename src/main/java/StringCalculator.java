2c39edf2f32913cbf722125f9469b442d3aca03f
Jack Bracey <jack.bracey@hotmail.co.uk>
2024-02-21 09:41:27 +0000

    Step 5

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public int Add(String numbers) {
+    public int Add(String numbers) throws Exception {
         if (numbers == null || numbers.equals(""))
             return 0;
 
@@ -11,6 +15,10 @@ public class StringCalculator {
             delimiter = numbers
                     .substring(0, numbers.indexOf("\n"))
                     .replaceAll("//", "");
+
+        List<String> negatives = findRegex(numbers, "(-[0-9])");
+        if (negatives.size() > 0)
+            throw new Exception("Negatives not allowed: " + String.join(",", negatives));
 
         if (numbers.contains("\n"))
             numbers = numbers.replaceAll("\n", delimiter);
@@ -24,6 +32,18 @@ public class StringCalculator {
                     }
                 })
                 .sum();
+    }
+
+    private List<String> findRegex(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> matches = new ArrayList<>();
+
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+        return matches;
     }
 
 }
--- src/test/java/StringCalculatorTests.java
+++ src/test/java/StringCalculatorTests.java
@@ -11,36 +11,49 @@ public class StringCalculatorTests {
 
     // Step one
     @Test
-    public void shouldReturnZeroOnEmptyString() {
+    public void shouldReturnZeroOnEmptyString() throws Exception {
         Assert.assertEquals(0, stringCalculator.Add(""));
     }
 
     @Test
-    public void shouldReturnThreeForZeroOneTwo() {
+    public void shouldReturnThreeForZeroOneTwo() throws Exception {
         Assert.assertEquals(3, stringCalculator.Add("0,1,2"));
     }
 
     // Step two
     @Test
-    public void shouldReturnOneHundredAndTwenty() {
+    public void shouldReturnOneHundredAndTwenty() throws Exception {
         Assert.assertEquals(120, stringCalculator.Add("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15"));
     }
 
     // Step three
     @Test
-    public void shouldReturnCorrectValueWithNewLinesInInput() {
+    public void shouldReturnCorrectValueWithNewLinesInInput() throws Exception {
         Assert.assertEquals(6, stringCalculator.Add("1\n2,3"));
     }
 
     @Test
-    public void shouldReturnCorrectValueWithNewLinesInInputTwo() {
+    public void shouldReturnCorrectValueWithNewLinesInInputTwo() throws Exception {
         Assert.assertEquals(1, stringCalculator.Add("1,\\n"));
     }
 
     // Step four
     @Test
-    public void shouldAllowForSpecifiedDelimiter() {
+    public void shouldAllowForSpecifiedDelimiter() throws Exception {
         Assert.assertEquals(3, stringCalculator.Add("//;\n1;2"));
+    }
+
+    // Step five
+    @Test
+    public void shouldNotAllowNegatives() {
+        Exception exception = Assert.assertThrows(Exception.class, () -> stringCalculator.Add("-1,2"));
+        Assert.assertEquals("Negatives not allowed: -1", exception.getMessage());
+    }
+
+    @Test
+    public void shouldNotAllowNegativesTwo() {
+        Exception exception = Assert.assertThrows(Exception.class, () -> stringCalculator.Add("2,-4,3,-5"));
+        Assert.assertEquals("Negatives not allowed: -4,-5", exception.getMessage());
     }
 
 }
