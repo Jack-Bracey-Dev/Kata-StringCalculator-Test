bf4b335156cc50fbadbb9f313b470cf214c1c52c
Jack Bracey <jack.bracey@hotmail.co.uk>
2024-02-21 09:31:45 +0000

    Step 2

--- src/test/java/StringCalculatorTests.java
+++ src/test/java/StringCalculatorTests.java
@@ -20,4 +20,10 @@ public class StringCalculatorTests {
         Assert.assertEquals(3, stringCalculator.Add("0,1,2"));
     }
 
+    // Step two
+    @Test
+    public void shouldReturnOneHundredAndTwenty() {
+        Assert.assertEquals(120, stringCalculator.Add("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15"));
+    }
+
 }
